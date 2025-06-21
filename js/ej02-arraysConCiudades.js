237b9a2a812c53e67b47db4df2c6be0987775148
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-21 17:00:51 -0300

    agrege la logica del ejercicio numero 2 que recibe una cadena de caracteres y lo convierte en una matriz y lo muestra en pantalla en las distitntas posiciones

--- js/ej02-arraysConCiudades.js
+++ js/ej02-arraysConCiudades.js
@@ -1,32 +1,55 @@
 const ciudades = [];
 let ciudad;
 
-do{
-ciudad = prompt('Ingrese una ciudad y su pais:  ');
-if(ciudad !== null){
-ciudades.push(ciudad);
-}
-}while(confirm('¿Desea ingresar otra ciudad?'));
+do {
+  ciudad = prompt(
+    "Ingrese ciudades y sus paises Ej: Nueva York, Estados unidos ; Barcelona, España:   "
+  );
+  //trim -> elimina espacion en blancos a ambos lados de la cadena
+  if (ciudad !== null && ciudad.trim !== ``) {
+  //split -> convierte una cadena en matriz 
+    let listaCiudades = ciudad.split(`;`);
+    ciudades.push(...listaCiudades);
+  }
+} while (confirm("¿Desea ingresar otra ciudad?"));
 
-if(ciudades.length > 0){
-document.writeln(`<p>El arreglo de cuidades tiene ${ciudades.length} elementos</p>`);
-console.log(`${ciudades.length}`)
-document.writeln(`<ul>`);
-if(ciudades.length>= 1 ){
-document.writeln(`<li> Elemento 1ra posicion: ${ciudades[0]} </li>`);
+if (ciudades.length > 0) {
+  document.writeln(
+    `<p>El arreglo de cuidades tiene ${ciudades.length} elementos</p>`
+  );
+
+  document.writeln(`<ul>`);
+  if (ciudades[0]) {
+    document.writeln(`<li> Elemento 1er posicion: ${ciudades[0]} </li>`);
+  }
+  if (ciudades[2]) {
+    document.writeln(`<li>Elemento 3er posicion: ${ciudades[2]} </li>`);
+  }
+  document.writeln(
+    `<li> Elemento ultima posicion: ${ciudades[ciudades.length - 1]} </li>`
+  );
+  document.writeln(`</ul>`);
 }
-if(ciudades.length >= 3){
-document.writeln(`<li>Elemento 3er posicion: ${ciudades[2]} </li>`)
-}
-if(ciudades.length >= 1){
-document.writeln(`<li> Elemento ultima posicion: ${ciudades[ciudades.length - 1]} <li>`);
-}
+
+ciudades.push("Paris");
+document.writeln("<ul>");
+document.writeln(
+  `<li> Elemento ultima posicion: ${ciudades[ciudades.length - 1]} </li>`
+);
 document.writeln(`</ul>`);
+document.writeln("<ul>");
+document.writeln(
+  `<li> Elemento que ocupa la segunda posicion: ${ciudades[1]} </li>`
+);
+document.writeln(`</ul>`);
+
+
 document.writeln(`<h2>Arreglos de ciudades</h2>`);
+ciudades.splice(1, 0, "Barcelona");
 document.writeln(`<ul>`);
-for(i=0; i< ciudades.length; i++){
-    document.writeln(`<li> ${ciudades[i]}</li>`);
+for (i = 0; i < ciudades.length; i++) {
+  document.writeln(`<li>Elemento: ${ciudades[i]}</li>`);
 }
 document.writeln(`</ul>`);
-}
+
 
